d9ee68c123edcd121f3dad5da7e383a02064e1e1
James Ribar <james@colemanfamilycamps.com>
2011-04-25 13:20:54 -0400

    Minor speed improvement for SelectView.  Let's only measure the length of the 5 longest strings instead of measuring all items, which for a several hundred item list is slow.

--- frameworks/desktop/views/select.js
+++ frameworks/desktop/views/select.js
@@ -624,7 +624,8 @@ SC.SelectView = SC.ButtonView.extend(
     className = customViewClassName ? (className + ' ' + customViewClassName) : className;
 
     SC.prepareStringMeasurement("", className);
-    for (idx = 0, itemsLength = items.length; idx < itemsLength; ++idx) {
+    var titles = items.getEach('title').sort(function(a,b) { return (a.length > b.length) ? -1 : 1;});
+    for (idx = 0, itemsLength = Math.min(titles.length, 5); idx < itemsLength; ++idx) {
       //getting the width of largest menu item
       item = items.objectAt(idx);
       elementOffsetWidth = SC.measureString(item.title).width;
